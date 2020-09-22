b59db1b464506387e394c0ea96fbf406a96257a6
werogg <supwer00@gmail.com>
2020-09-22 21:10:29 +0200

    permissions bug fix

--- src/main/java/es/jotero/skybags/permissions/BagPermissions.java
+++ src/main/java/es/jotero/skybags/permissions/BagPermissions.java
@@ -3,7 +3,7 @@ package es.jotero.skybags.permissions;
 public class BagPermissions {
 
     public static final String COMMAND_BAG = "skybags.command.bag";
-    public static final String COMMAND_BAG_SPEC = "skybags.command.bag.spec";
+    public static final String COMMAND_BAG_SPEC = "skybags.command.spectate.bag";
     public static final String COMMAND_BAG_SIZE_ONE = "skybags.command.bagsize.one";
     public static final String COMMAND_BAG_SIZE_TWO = "skybags.command.bagsize.two";
     public static final String COMMAND_BAG_SIZE_THREE = "skybags.command.bagsize.three";
